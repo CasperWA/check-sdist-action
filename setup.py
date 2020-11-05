a0e69350076af6fe97533806ff4995c555d91dca
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-11-05 19:07:51 +0100

    Create mock Python repository "good_package"
    This mock package will successfully build and install a source
    distribution.

--- setup.py
+++ setup.py
@@ -4,22 +4,23 @@ from setuptools import setup, find_packages
 
 
 TOP_DIR = Path(__file__).parent.resolve()
+PACKAGE_NAME = "good_package"
 
-with open(TOP_DIR.joinpath("success_package/__init__.py"), "r") as handle:
+with open(TOP_DIR.joinpath(f"{PACKAGE_NAME}/__init__.py"), "r") as handle:
     for line in handle.readlines():
         version = re.findall(r'__version__ = "(.*)"', line)
         if version:
             break
     else:
-        raise RuntimeError("Could not determine version from success_package/__init__.py")
+        raise RuntimeError(f"Could not determine version from {PACKAGE_NAME}/__init__.py")
 
 with open(TOP_DIR.joinpath("requirements.txt")) as handle:
     REQUIREMENTS = handle.read()
 
 setup(
-    name="success_package",
+    name=PACKAGE_NAME,
     version=version[0],
-    description="This example package will successfully build a source distribution.",
+    description="This example package will successfully build and install a source distribution.",
     packages=find_packages(),
     install_requires=REQUIREMENTS,
 )
